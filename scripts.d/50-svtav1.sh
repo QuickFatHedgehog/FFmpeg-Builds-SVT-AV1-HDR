fc9b1c5ae22da7f1f78e3263eb8ce9e1ca12185c
Uranite <62639703+Uranite@users.noreply.github.com>
2024-10-03 21:59:21 +0700

    svt-av1-hdr c365615

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://github.com/BlueSwordM/svt-av1-psyex.git"
-SCRIPT_COMMIT="dc4d11db0e7099f24b1e1dcfc24ca8361f77e5c7"
-SCRIPT_BRANCH="testing"
+SCRIPT_REPO="https://github.com/juliobbv-p/svt-av1-hdr.git"
+SCRIPT_COMMIT="c365615d0e106de3a0d0ba4ce6f14ec7d5fde466"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
