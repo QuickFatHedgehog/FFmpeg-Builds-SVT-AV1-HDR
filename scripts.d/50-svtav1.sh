Uranite/FFmpeg-Builds-SVT-AV1-PSY

svt-av1-psyex v3.0.2-B

scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SCRIPT_COMMIT="724d061f5d6463bd1061595d7004747cd01a480a"
+SCRIPT_REPO="https://github.com/BlueSwordM/svt-av1-psyex.git"
+SCRIPT_COMMIT="e18a905fc3632226a473235d2d6c4c544959e318"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1

QuickFatHedgehog/FFmpeg-Builds-SVT-AV1-HDR

scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SCRIPT_COMMIT="fb0ed7e5999caef1c4b51b7b4c8dc2c8c10f9291"
+SCRIPT_REPO="https://github.com/BlueSwordM/svt-av1-psyex.git"
+SCRIPT_COMMIT="fedc23378247b6ab504cbcdfedf428efab9ad452"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1